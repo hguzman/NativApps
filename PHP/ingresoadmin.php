f214ec0138dbd36b169129a15f3c206dff5a3e9e
Mario_Gomez <57111043+MERZIOX@users.noreply.github.com>
2020-05-26 15:28:31 -0500

    Agrego plantilla de trabajo

---
+++ PHP/ingresoadmin.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <link rel="stylesheet" href="CSS\bootstrap.css">
+    <link rel="stylesheet" href="CSS\style.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>INICIO</title>
+</head>
+
+<body>
+
+    <div class="container-fluid" id="contenedor">
+
+        <head class="cabecera">
+            <nav>
+                <ul class="nav justify-content-center">
+                    <li class="nav-item">
+                        <a class="nav-link a-actual" href="">Ingresar equipos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="">ver equipos</a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link " href="">ver manual tecnico</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">ver manual usuario</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="">salir de la sesion</a>
+                    </li>
+                </ul>
+            </nav>
+        </head>
+        <div>
+
+
+        </div>
+
+    </div>
+
+
+</body>
+
+</html>
