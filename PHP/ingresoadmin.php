ad44e434bc78f38c6549326e88387f9ac099fd58
LivanTorresP <55635469+Dimiorz@users.noreply.github.com>
2020-07-13 23:58:47 -0500

    Revert "Cambie datos en el form y eliminé archivos inutiles"

--- PHP/ingresoadmin.php
+++ PHP/ingresoadmin.php
@@ -10,8 +10,6 @@
     <title>INICIO</title>
 </head>
 <script src="..\JS\jquery-3.4.1.min.js"></script>
-<script src="..\JS\menu-equipo.js"></script>
-<script src="..\JS\menu-perifericos.js"></script>
 
 <body>
 
@@ -22,7 +20,7 @@
                 <ul class="nav justify-content-center">
 
                     <div class="dropdown">
-                        <a href="" class="btn btn-success dropdown-toggle" role="button" id="btn-menu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Equipos </a>
+                        <a href="" class="btn btn-success dropdown-toggle" role="button" id="btn-menu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Software </a>
 
                         <div class="dropdown-menu" aria-labelledby="btn-menu1">
                             <a id="ingre-equip" class="dropdown-item ">Ingresar equipo</a>
@@ -31,7 +29,7 @@
                         </div>
                     </div>
                     <li class="nav-item">
-                        <p class="nav-link text-1" href=""> | </p>
+                        <a class="nav-link" href=""> | </a>
                     </li>
                     <div class="dropdown">
                         <a href="" class="btn btn-success dropdown-toggle" role="button" id="btn-menu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Usuarios </a>
@@ -44,7 +42,7 @@
                         </div>
                     </div>
                     <li class="nav-item">
-                        <p class="nav-link text-1" href=""> | </p>
+                        <a class="nav-link" href=""> | </a>
                     </li>
                     <div class="dropdown">
                         <a href="" class="btn btn-success dropdown-toggle" role="button" id="btn-menu1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">Manuales </a>
@@ -57,7 +55,7 @@
                         </div>
                     </div>
                     <li class="nav-item">
-                        <p class="nav-link text-1" href=""> | </p>
+                        <a class="nav-link" href=""> | </a>
                     </li>
 
                     <li class="nav-item">
@@ -67,212 +65,132 @@
             </nav>
         </header>
 
-
+        
         <div style="position: relative; background-color: chartreuse; width: 60">
             <div id="inicial" class="position-absolute-pasada"></div>
-
+           
             <div id="ingresar-equipos" class="off-visibily position-absolute-pasada">
-
+               
                 <h2 class="text-center" style="color: gainsboro">REGISTRO</h2>
+                <br>
                 <form action="formulario.php" method="post" class="validate" id="formulario">
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <label class="text-label" for="mascota"> Equipo:</label>
-                            <input type="text" class="form-control" placeholder="">
-                        </div>
 
-                        <div class="col-lg-6">
-                            <label class="text-label" for="Serial"> Serial:</label>
-                            <input type="number" class="form-control" placeholder="">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <label class="text-label" for="Marca"> Marca:</label>
-                            <input type="text" class="form-control" placeholder="">
-                        </div>
-                        <div class="col-lg-6">
-                            <label class="text-label" for="Nombre"> Nombre:</label>
-                            <input type="text" class="form-control" placeholder="">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <label class="text-label" for="Tipo"> Tipo_de_equipo:</label>
-                            <select name="" id="" class="form-control" placeholder="">
-                                <option selected value="Seleccionar">Selecciona</option>
-                                <option value="Portatil"> Portatil</option>
-                                <option value="Escritorio">Escritorio</option>
-                                <option value="Todo en uno">Todo en uno</option>
-                            </select>
-                        </div>
-                        <div class="col-lg-6">
-                            <label class="text-label" for="Modelo"> Modelo del equipo:</label>
-                            <input type="number" class="form-control" placeholder="">
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <label class="text-label" for="Procesador"> Procesador:</label>
-                            <input type="text" class="form-control" placeholder="">
-                        </div>
-                        <div class="col-lg-6">
-                            <label class="text-label" for="RAM">Memoria_RAM:</label>
-                            <input type="number" class="form-control" placeholder="">
+            <label for="mascota"> Equipo:</label> 
+            <input type="text" placeholder="">
+            <P></P>
+     <label for="Serial"> Serial:</label>
+            <input type="number" placeholder="">
+            <P></P>
+            <label for="Marca"> Marca:</label>
+            <input type="text" placeholder="">
+            <P></P>
+            <label for="Nombre"> Nombre:</label>
+            <input type="text" placeholder="">
+            <p></p>
+            <label for="Tipo"> Tipodeequipo:</label>
+            <input type="text" placeholder="">
+            <p></p>
+            <label for="Modelo"> Modelodelequipo:</label>
+            <input type="number" placeholder="">
+            <P></P>
+            <label for="Procesador"> Procesador:</label>
+            <input type="text" placeholder="">
+            <P></P>
+            <label for="RAM"> RAM:</label>
+            <input type="number" placeholder="">
+            <P></P>
+            <label for="Disco"> Disco:</label>
+            <input type="text" placeholder="">
+            <P></P>
+            <label for="Licencia"> Lic_Windows:</label>
+            <input type="text" placeholder="">
+            <P></P>
 
-                        </div>
-                    </div>
-
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <label class="text-label" for="Disco"> Disco_duro:</label>
-                            <input type="text" class="form-control" placeholder="">
-                        </div>
-                        <div class="col-lg-6">
-                            <label class="text-label" for="Licencia"> Lic_Windows:</label>
-                            <input type="text" class="form-control" placeholder="">
-                        </div>
-
-                    </div>
-                    <br>
-                    <div class="card text-center">
-                        <div class="card-header btn-group" role="group">
-                            <ul class="nav nav-pills card-header-pills">
-                                <li class="nav-item">
-                                    <a id="ini" class="nav-link text-2 active" href="#inicio"> Inicio </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a id="mou" class="nav-link text-2" href="#mouse"> Mouse </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a id="tec" class="nav-link text-2 " href="#teclado"> Teclado </a>
-                                </li>
-                                <li class="nav-item ">
-                                    <a id="enf" class="nav-link text-2" href="#enfriadora"> Enfriadora </a>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div id="perifericos" class="card-body off-visibily position-relative">
-
-                            <div id="inicio" class="row position-absolute-actual off-visibily" style="margin-top: 1%;">
-                                <h5>Aqui podras editar los perifericos</h5>
-                            </div>
-
-                            <div id="mouse" class="card-body off-visibily position-absolute-pasada">
-                                <div class="row">
-                                    <div class="col-4">
-                                        <label for="">marca</label>
-                                        <input class="form-control" type="text" name="mou-marca" id="">
-                                    </div>
-
-                                    <div class="col-4">
-                                        <label for="">modelo</label>
-                                        <input class="form-control" type="text" name="mou-modelo" id="">
-                                    </div>
-
-                                    <div class="col-4">
-                                        <label for="">linea</label>
-                                        <input class="form-control" type="text" name="mou-linea" id="">
-                                    </div>
-                                </div>
-
-                            </div>
-                            <div id="teclado" class="card-body off-visibily position-absolute-pasada">
-                                <div class="row">
-                                    <div class="col-4">
-                                        <label for="">marca</label>
-                                        <input class="form-control" type="text" name="tec-marca" id="">
-                                    </div>
-
-                                    <div class="col-4">
-                                        <label for="">modelo</label>
-                                        <input class="form-control" type="text" name="tec-modelo" id="">
-                                    </div>
-
-                                    <div class="col-4">
-                                        <label for="">linea</label>
-                                        <input class="form-control" type="text" name="tec-linea" id="">
-                                    </div>
-                                </div>
-
-                            </div>
-
-                            <div id="enfriadora" class="card-body off-visibily position-absolute-pasada">
-                                <div class="row">
-                                    <div class="col-4">
-                                        <label for="">marca</label>
-                                        <input class="form-control" type="text" name="enf-marca" id="">
-                                    </div>
-
-                                    <div class="col-4">
-                                        <label for="">modelo</label>
-                                        <input class="form-control" type="text" name="enf-modelo" id="">
-                                    </div>
-
-                                    <div class="col-4">
-                                        <label for="">linea</label>
-                                        <input class="form-control" type="text" name="enf-linea" id="">
-                                    </div>
-
-                                </div>
-                            </div>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-                            <br>
-
-                            <div class="row">
-                                <div class="col-lg-6">
-                                    <button id="valid" class="control-form btn btn-success btn-block" type="submit" value="">REGISTRAR </button>
-                                </div>
-                                <div class="col-lg-6">
-                                    <button id="valid" class="control-form btn btn-danger btn-block" type="submit" value="">LIMPIAR FORM</button>
-                                </div>
-                            </div>
-
-                </form>
-
+            <input id="valid" type="submit" value="REGISTRAR">
+                 </form>
+            
             </div>
 
             <div id="buscar-equipos" class="off-visibily position-absolute-pasada">
-
+                
                 <h2 class="text-center" style="color: gainsboro">BUSCAR EQUIPO</h2>
                 <br>
                 <form action="formulario.php" method="post" class="validate" id="formulario">
 
-
-
+                  
+                
             </div>
 
 
             <div id="modificar-equipos" class="off-visibily position-absolute-pasada">
-
+                
 
                 <h2 class="text-center" style="color: gainsboro">MODIFICAR EQUIPO</h2>
                 <br>
                 <form action="formulario.php" method="post" class="validate" id="formulario">
 
-
-
-            </div>
-
-
+                  
+                
 
         </div>
+    </div>
 
 
 
+    <script>
+        $(Document).ready(function() {
+            alert("Ya puedes editar el jquery");
+
+            $('#ingre-equip').click(function() {
+                    $('#buscar-equipos').removeClass("on-visibily")
+                    $('#buscar-equipos').addClass("off-visibily")
+
+                    $('#modificar-equipos').removeClass("on-visibily")
+                    $('#modificar-equipos').addClass("off-visibily")
+
+                    $('#ingresar-equipos').addClass("position-absolute-actual");
+                    $('#ingresar-equipos').removeClass("off-visibily")
+                    $('#ingresar-equipos').addClass("on-visibily")
+                }
+
+            )
+
+            $('#busc-equip').click(function() {
+
+                $('#ingresar-equipos').removeClass("on-visibily")
+                $('#ingresar-equipos').addClass("off-visibily")
+
+                $('#modificar-equipos').removeClass("on-visibily")
+                $('#modificar-equipos').addClass("off-visibily")
+
+                $('#buscar-equipos').addClass("position-absolute-actual");
+                $('#buscar-equipos').removeClass("off-visibily")
+                $('#buscar-equipos').addClass("on-visibily")
+
+            })
+
+            $('#modif-equip').click(function() {
+                $('#buscar-equipos').removeClass("on-visibily")
+                $('#buscar-equipos').addClass("off-visibily")
+                
+                $('#ingresar-equipos').removeClass("on-visibily")
+                $('#ingresar-equipos').addClass("off-visibily")
+
+                $('#modificar-equipos').addClass("position-absolute-actual");
+                $('#modificar-equipos').removeClass("off-visibily");
+                $('#modificar-equipos').addClass("on-visibily");
+
+            })
+        });
+    </script>
 
 
 
-
-        <!--Archivos de javascript -->
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-        <!--Archivos de javascript-->
+    <!--Archivos de javascript -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <!--Archivos de javascript-->
 </body>
 
 </html>
