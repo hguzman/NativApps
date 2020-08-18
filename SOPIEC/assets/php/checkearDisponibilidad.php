62955097982107226e37024115a3546450a674e7
dimiorz <livantorresp@gmail.com>
2020-08-18 01:47:31 -0500

    Cambios: crud- alertas - formulario equipos
    *se añadieron validaciones JS añadir equipos
    *se cambiaron las alertas
    *se agrego el campo estado al formulario de esa
    *se agrego a la tabla de equipo la vista de estados

--- SOPIEC/assets/php/checkearDisponibilidad.php
+++ SOPIEC/assets/php/checkearDisponibilidad.php
@@ -1,6 +1,7 @@
 <?php
 require('db.php');
 sleep(1);
+//Validar si la cedula existe en la BD
 if (isset($_POST['cedula'])) {
     $cedula = (string)$_POST['cedula'];
 
@@ -14,7 +15,7 @@ if (isset($_POST['cedula'])) {
         echo '<div id="disponibilidad" class="alert alert-success" value="si"><strong>Enhorabuena!</strong>Esta cedula está disponible.</div>';
     }
 }
-
+//Validar si el email existe en la BD
 if (isset($_POST['email'])) {
     $email = (string)$_POST['email'];
 
@@ -26,5 +27,20 @@ if (isset($_POST['email'])) {
         echo '<div id="disponibilidad" class="alert alert-danger" value="no"><strong>Correo registrado!</strong> Este correo ya se encuentra registrado.</div>';
     } else {
         echo '<div id="disponibilidad" class="alert alert-success" value="si"><strong>Enhorabuena!</strong>Este correo está disponible.</div>';
+    }
+   
+}
+//Validar si el serial existe en la BD
+if (isset($_POST['serial'])) {
+    $serial = (string)$_POST['serial'];
+
+    $result = $conexion->query(
+        'SELECT * FROM equipos WHERE serial = "' . strtolower($serial) . '"'
+    );
+
+    if ($result->num_rows > 0) {
+        echo '<div id="disponibilidad" class="alert alert-danger" value="no"><strong>Serial registrado!</strong> Este serial ya se encuentra registrado.</div>';
+    } else {
+        echo '<div id="disponibilidad" class="alert alert-success" value="si"><strong>Enhorabuena!</strong>Este serial está disponible.</div>';
     }
 }
