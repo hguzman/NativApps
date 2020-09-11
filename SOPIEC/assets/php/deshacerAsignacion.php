ae0ca6f2c0fa1dae9b3b3f6c61ab9bf654f9f389
Mario_Gomez <57111043+MERZIOX@users.noreply.github.com>
2020-09-11 03:35:53 -0500

    errores de borrado de asignacion

--- SOPIEC/assets/php/deshacerAsignacion.php
+++ SOPIEC/assets/php/deshacerAsignacion.php
@@ -4,8 +4,9 @@ require_once("val_session_admin.php");
 require_once("db.php");
 
 $cedula = $_GET['cedula'];
+$serial = $_GET['serial'];
 
-$registros = mysqli_query($conexion, "update equipos set cedula_FK=0 where cedula_FK='$_REQUEST[cedula]'") or
+$registros = mysqli_query($conexion, "update equipos set cedula_FK=0 where cedula_FK='$_REQUEST[cedula]' and serial= '$_REQUEST[serial]'") or
 die("Problemas en el select:" . mysqli_error($conexion));
 
 header('Location: ../../equiposAsignados.php');
--- SOPIEC/equiposAsignados.php
+++ SOPIEC/equiposAsignados.php
@@ -1,7 +1,7 @@
 <?php
 require_once("assets/php/db.php");
 require_once("assets/php/val_session_admin.php");
-$cc= "SELECT cedula FROM usuarios WHERE cedula='$sesion'";
+$cc = "SELECT cedula FROM usuarios WHERE cedula='$sesion'";
 ?>
 
 <!DOCTYPE html>
@@ -66,8 +66,7 @@ $cc= "SELECT cedula FROM usuarios WHERE cedula='$sesion'";
                     </li>
                     <!-- Segundo/Administracion de usuarios -->
                     <li>
-                        <a href="#"><i class="fa fa-edit "></i>Administración de usuarios<span
-                                class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-edit "></i>Administración de usuarios<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
                                 <a href="nuevousuario.php">Crear Nuevo usuario</a>
@@ -79,8 +78,7 @@ $cc= "SELECT cedula FROM usuarios WHERE cedula='$sesion'";
                     </li>
                     <!-- Cuarto/Administracion de equipos -->
                     <li>
-                        <a href="#"><i class="fa fa-sitemap "></i>Administración de equipos<span
-                                class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-sitemap "></i>Administración de equipos<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
                                 <a href="nuevoequipo.php">Agregar un equipo</a>
@@ -110,20 +108,17 @@ $cc= "SELECT cedula FROM usuarios WHERE cedula='$sesion'";
                         <div class="usuarios-buscar">
                             <h2>Equipos Asignados</h2>
                             <!-- Barra de busqueda -->
-                            <form class="form-inline my-2 my-lg-0 barra-buscar" action="assets/php/buscarusuario.php"
-                                method="GET">
-                                <input class="form-control mr-sm-2" type="search" placeholder="C.C" aria-label="Search"
-                                    id="buscar_usuario" name="buscar_usuario">
-                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="boton_buscar"
-                                    id="boton_buscar">Buscar</button>
+                            <form class="form-inline my-2 my-lg-0 barra-buscar" action="assets/php/buscarusuario.php" method="GET">
+                                <input class="form-control mr-sm-2" type="search" placeholder="C.C" aria-label="Search" id="buscar_usuario" name="buscar_usuario">
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit" name="boton_buscar" id="boton_buscar">Buscar</button>
                             </form>
                         </div>
                     </div>
                 </div>
                 <hr />
-                <?php 
-        $query_asignacion = mysqli_query($conexion,"select cedula,primer_nombre,primer_apellido,serial,marca,nombre from equipos,usuarios where cedula=cedula_FK") or die("Problemas en el select:" . mysqli_error($conexion));
-            ?>
+                <?php
+                $query_asignacion = mysqli_query($conexion, "select cedula,primer_nombre,primer_apellido,serial,marca,nombre from equipos,usuarios where cedula=cedula_FK") or die("Problemas en el select:" . mysqli_error($conexion));
+                ?>
                 <div id="contenedor-usuarios" class="contenedor-usuarios">
                     <div class="row contenedor-tabla">
                         <!-- Tabla de valores en base de datos -->
@@ -141,33 +136,30 @@ $cc= "SELECT cedula FROM usuarios WHERE cedula='$sesion'";
                             </thead>
                             <tbody>
                                 <?php
-                                 while($regas = mysqli_fetch_array($query_asignacion)){
+                                while ($regas = mysqli_fetch_array($query_asignacion)) {
                                 ?>
-                                <!-- Contenido de la tabla -->
-                                <tr class="actual">
-                                    <th scope="row">
-                                        <input name="cedula" class="form-control" type="number"
-                                            value="<?php echo $cedula = $regas['cedula'] ?>" readonly id="cedula"
-                                            name="cedula" readonl> </th>
-                                    <td> <?php echo $regas['primer_nombre'] ?></td>
-                                    <td> <?php echo $regas['primer_apellido'] ?> </td>
-                                    <td id="seri"> <?php echo $regas['serial'] ?> </td>
-                                    <td> <?php echo $regas['marca'] ?> </td>
-                                    <td> <?php echo $regas['nombre'] ?> </td>
-                                    <td class="eliminar-editar">
-                                        <!-- botones editar y eliminar -->
-                                        <a id="del" class="btn fa fa-trash-alt" href="#"
-                                            onclick="confirmacion_borrar(<?php echo $regas['cedula']; ?>)"></a>
-                                    </td>
-                                </tr>
+                                    <!-- Contenido de la tabla -->
+                                    <tr class="actual">
+                                        <th scope="row">
+                                            <input name="cedula" class="form-control" type="number" value="<?php echo $cedula = $regas['cedula'] ?>" readonly id="cedula" name="cedula" readonl> </th>
+                                        <td> <?php echo $regas['primer_nombre'] ?></td>
+                                        <td> <?php echo $regas['primer_apellido'] ?> </td>
+                                        <td id="seri"> <?php echo $regas['serial'] ?> </td>
+                                        <td> <?php echo $regas['marca'] ?> </td>
+                                        <td> <?php echo $regas['nombre'] ?> </td>
+                                        <td class="eliminar-editar">
+                                            <!-- botones editar y eliminar -->
+                                            <a id="del" class="btn fa fa-trash-alt" href="#" onclick="confirmacion_borrar('<?php echo $regas['cedula']; ?>.<?php echo $regas['serial'] ?>')"></a>
+                                        </td>
+                                    </tr>
                     </div>
                 </div>
-                <?php
+            <?php
                                 }
                                 mysqli_close($conexion);
             ?>
-                </tbody>
-                </table>
+            </tbody>
+            </table>
             </div>
 
             <!-- /. ROW  -->
@@ -188,16 +180,20 @@ $cc= "SELECT cedula FROM usuarios WHERE cedula='$sesion'";
     <script src="assets/js/custom.js"></script>
     <script src="assets/js/validaciones.js"></script>
     <!-- CDN Jquery-->
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js"
-        integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <!-- Script Toastr -->
     <script src="//cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
     <script src="assets/js/validaciones.js"></script>
 
     <!-- Alerta borrar -->
     <script type="text/javascript">
-        function confirmacion_borrar(cedula) {
-            if (confirm(`¿Realmente desea deshacer la asignacion de equipo del usuario C.C ${cedula}?`)) {
+        function confirmacion_borrar(texto) {
+
+            var separado = texto.split(".");
+            var cedula = separado[0];
+            var serial = separado[1];
+
+            if (confirm(`¿Realmente desea deshacer la asignacion de equipo del usuario C.C ${cedula}? con el serial ${serial}?`)) {
 
                 toastr["error"]("Se ha destruido la asignación de equipo al usuario C.C #" + cedula + ".",
                     "Asignación eliminada")
@@ -219,7 +215,7 @@ $cc= "SELECT cedula FROM usuarios WHERE cedula='$sesion'";
                     "hideMethod": "fadeOut"
                 }
                 setTimeout(() => {
-                    window.location.href = "assets/php/deshacerAsignacion.php?cedula=" + cedula
+                    window.location.href = "assets/php/deshacerAsignacion.php?cedula=" + cedula + "&" + "serial=" + serial
                 }, 2000);
             }
         }
