1dfad41dbf6437713606a08d7364a5d82f8f4255
Mario_Gomez <57111043+MERZIOX@users.noreply.github.com>
2020-09-19 03:17:11 -0500

    issues #157 y #158 Solucionados
    #157: ahora todas las vistas se rigen unicamente por dos archivos en la carpeta modelos (a excepcion de vista_usuario.php y perfil.php se buscará pronta solucion a ese bug), haciendo más facil el cambio masivo en la estructura head y sidebar
    #158: Ahora el avatar compara que la cedula sea igual a la sesion actual para hacer un solo cambio

--- SOPIEC/assets/modelos/navbar_header_admin_vs.php
+++ SOPIEC/assets/modelos/navbar_header_admin_vs.php
@@ -15,8 +15,8 @@
                         <li><a>Rol: <?php echo $rol = $_SESSION['rol']; ?> </a></li>
                         <li><a>Sesion: <?php echo $sesion = $_SESSION['username']; ?> </a></li>
                         <li><a href="#">See Website</a></li>
-                        <li><a href="ticket.php">Open Ticket</a></li>
-                        <li><a href="assets/php/logout.php">Cerrar sesión</a></li>
+                        <li><a href="../../ticket.php">Open Ticket</a></li>
+                        <li><a href="../php/logout.php">Cerrar sesión</a></li>
                     </ul>
                 </div>
             </div>
@@ -26,44 +26,46 @@
         <nav class="navbar-default navbar-side" role="navigation">
             <div class="sidebar-collapse">
                 <ul class="nav" id="main-menu">
-                    <!-- recuperar imagen de la base de datos -->
-                    <?php include("assets/modelos/avatar_admin.php"); ?>
+                        <!-- recuperar imagen de la base de datos -->
+                        <?php include_once("../modelos/avatar_admin_vs.php"); ?> 
                     <!-- Primero/inicio -->
                     <li>
-                        <a href="index.php"><i class="fa fa-desktop "></i>Inicio</a>
+                        <a href="../../index.php"><i class="fa fa-desktop "></i>Inicio</a>
                     </li>
                     <!-- Segundo/Administracion de usuarios -->
                     <li>
-                        <a href="#"><i class="fa fa-edit "></i>Administración de usuarios<span class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-edit "></i>Administración de usuarios<span
+                                class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
-                                <a href="nuevousuario.php">Crear Nuevo usuario</a>
+                                <a href="../../nuevousuario.php">Crear Nuevo usuario</a>
                             </li>
                             <li>
-                                <a href="usuarios.php">Ver usuarios registrados</a>
+                                <a href="../../usuarios.php">Ver usuarios registrados</a>
                             </li>
                         </ul>
                     </li>
                     <!-- Cuarto/Administracion de equipos -->
                     <li>
-                        <a href="#"><i class="fa fa-sitemap "></i>Administración de equipos<span class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-sitemap "></i>Administración de equipos<span
+                                class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
-                                <a href="nuevoequipo.php">Agregar un equipo</a>
+                                <a href="../../nuevoequipo.php">Agregar un equipo</a>
                             </li>
                             <li>
-                                <a href="equipos.php">Gestionar equipo</a>
+                                <a href="../../equipos.php">Gestionar equipo</a>
                             </li>
 
                         </ul>
                     </li>
                     <!--  Quinto/ver tickets-->
                     <li>
-                        <a href="mostrartickets.php"><i class="fa fa-qrcode "></i>Tickets</a>
+                        <a href="../../mostrartickets.php"><i class="fa fa-qrcode "></i>Tickets</a>
                     </li>
                     <!--  Sexto/A cerca de SOPIEC-->
                     <li>
-                        <a href="manuales.html"><i class="fa fa-qrcode "></i>A cerca de SOPIEC</a>
+                        <a href="../../manuales.html"><i class="fa fa-qrcode "></i>A cerca de SOPIEC</a>
                     </li>
                 </ul>
 
