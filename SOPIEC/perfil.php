82f2a8ffa942f1baca21b60f179bce6542bef600
Kelvin <kmr19972015@gmail.com>
2020-11-03 12:24:24 -0500

    Verificación de correo al actualizar
    - Ya no se permite registrar ningún correo existente en la BD
    - En caso de que el usuario intente hacerlo será redirigido a una pagina de error indicandole el problema.

--- SOPIEC/perfil.php
+++ SOPIEC/perfil.php
@@ -74,7 +74,12 @@ require_once("assets/php/val_session_admin.php");
                         <center>
                             <button id="asignar" data-toggle="modal" data-target="#staticBackdrop" href="modal"
                                 class="btn btn-success btn-sm r" name="modificarUser"
-                                style="border-radius: 5px !important;">Actualizar mis datos</button></center>
+                                style="border-radius: 5px !important;">Actualizar mis datos</button>
+                            <!-- btn Cambiar Email -->
+                            <button id="asignar" data-toggle="modal" data-target="#cambiarEmail" href="modal"
+                                class="btn btn-info btn-sm r" name="cambiarEmail"
+                                style="border-radius: 5px !important;">Cambiar E-mail</button>
+                        </center>
                     </div>
 
                     <!-- Form Actualizar contraseña -->
@@ -180,14 +185,7 @@ require_once("assets/php/val_session_admin.php");
                                                     value="<?php echo $reg['segundo_apellido'] ?>">
                                             </div>
                                             <!-- Email -->
-                                            <div class="form-group col-md-6">
-                                                <label for="registrar-email">Email</label>
-                                                <input type="email" class="form-control" id="email" name="email"
-                                                    placeholder="correo_143@correo.com" require
-                                                    value="<?php echo $reg['email'] ?>">
-                                                <!-- Div de carga -->
-                                                <div id="result-email"></div>
-                                            </div>
+
 
 
                                             <div class="form-group col-md-12">
@@ -207,6 +205,65 @@ require_once("assets/php/val_session_admin.php");
                                                 data-dismiss="modal">Cancelar</button>
                                             <input type="submit" class="btn btn-primary r" value="Actualizar datos"
                                                 name="actualizarDatosAdmin">
+                                        </div>
+                                    </form>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <!-- Modal actualizar Email-->
+                    <div class="modal fade" id="cambiarEmail" data-backdrop="static" data-keyboard="false" tabindex="-1"
+                        aria-labelledby="staticBackdropLabel" aria-hidden="true">
+
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="staticBackdropLabel">Cambiar E-mail</h5>
+                                    <button type="button" class="close r" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <!-- Contenido del modal -->
+                                <div class="modal-body">
+                                    <form method="POST" id="cambiarEmail" name="cambiarEmail"
+                                        action="assets/php/actualizarDatosAdmin.php">
+                                        <div class="form-row">
+
+                                            <!-- Email actual-->
+                                            <div class="form-group col-md-12">
+                                                <label for="registrar-email">Email actual</label>
+                                                <input readonly type="email" class="form-control" id="" name=""
+                                                    placeholder="" value="<?php echo $reg['email'] ?>">
+                                            </div>
+                                            <!-- Nuevo Email -->
+                                            <div class="form-group col-md-12">
+                                                <label for="registrar-email">Nuevo Email</label>
+                                                <input type="email" class="form-control" id="email"
+                                                    name="nuevoEmailAdmin" placeholder="correo_143@correo.com" required
+                                                    value="">
+                                                <!-- Div de carga -->
+                                                <div id="result-email"></div>
+                                            </div>
+
+                                            <div class="form-group col-md-12">
+                                                <label for="contrasenaActual">Contraseña actual</label>
+                                                <br>
+                                                <div class="alert alert-warning" role="alert">
+                                                    Este campo es obligatorio para efecuar los
+                                                    cambios.</div>
+                                                <input type="password" class="form-control" id="contrasenaActual"
+                                                    name="contrasenaActual" placeholder="*********" required value=""
+                                                    style="margin-bottom: 1rem;">
+                                            </div>
+                                        </div>
+                                        <!-- Botones -->
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary r"
+                                                data-dismiss="modal">Cancelar</button>
+                                            <input type="submit" class="btn btn-primary r" value="Actualizar datos"
+                                                name="cambiarEmail">
                                         </div>
                                     </form>
 
